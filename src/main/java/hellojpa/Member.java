cf1472d5eec32bc6ea51d1c5f905c69ee4ba1e71
rewritech <rwtstar21@gmail.com>
2022-05-13 08:42:27 +0900

    add sequence

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,9 +5,18 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 @Entity
+//@SequenceGenerator(
+//        name = “MEMBER_SEQ_GENERATOR",
+//        sequenceName = “MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+//        initialValue = 1, allocationSize = 1)
 //@Table(UniqueConstraint=) 유니크 제약은 보통 여기에 준다
 public class Member {
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에 위임: 자동 생성 1,2,3,4 ...
+    // DB 시퀀스 오프젝트 생성 & 이용. int, Integer 사용하지 않음 10억 넘으면 초기화
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Integer
     private Long id;
 
     // unique도 있지만 이름이 랜덤으로 지정되어 에러시 파악 어렵기에 안 씀
