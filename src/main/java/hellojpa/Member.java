b80f68b5e28726f0564cabfbda8ba5fce871d003
rewritech <rwtstar21@gmail.com>
2022-05-12 22:22:36 +0900

    add insertable, updatable, nullable, unique

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,13 +5,16 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 @Entity
+//@Table(UniqueConstraint=) 유니크 제약은 보통 여기에 준다
 public class Member {
     @Id
     private Long id;
 
-    @Column(name = "name")
+    // unique도 있지만 이름이 랜덤으로 지정되어 에러시 파악 어렵기에 안 씀
+    @Column(name = "name", nullable = false, updatable = false)
     private String username;
 
+    @Column()
     private Integer age;
 
     @Enumerated(EnumType.STRING)    // DB에 enum 없기에 지정
