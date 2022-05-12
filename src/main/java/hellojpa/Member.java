e195198ed342a6f72fd82f99ebd0c1dde0185c65
rewritech <rwtstar21@gmail.com>
2022-05-12 13:00:39 +0900

    add entity table mapping & DDL

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,16 +1,19 @@
 package hellojpa;
 
 //import javax.persistence.Column;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 //import javax.persistence.Table;
 
 @Entity
-//@Table(name = "Member")
+//@Entity(name = "Member")  // 클래스명이 기본값. 다른 패키지에 동일 클래스명이 있을 때 지정
+//@Table(name = "Member")   // 테이블명 지정
 public class Member {
     @Id
     private Long id;
 //    @Column(name = "username")
+    @Column(unique = true, length = 10)
     private String name;
 
     public Member() {}
