cb97dd644fe3d7fd3b56e2af056fcb3a83315060
rewritech <rwtstar21@gmail.com>
2022-05-14 09:43:38 +0900

    Add Id 전략

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -14,12 +14,14 @@ import java.util.Date;
         table = "MY_SEQUENCES",
         pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
+    // Id는 시스템이 사라지는 아주 먼 미래까지 변해서는 안된다.
+    // Long + 대체키 + 키 혹은 시퀀스 혹은 자동증가생성을 이용하자
     @Id
 //    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB에 위임: 자동 생성 1,2,3,4 ...
     // DB 시퀀스 오프젝트 생성 & 이용. int, Integer 사용하지 않음 10억 넘으면 초기화
 //    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     // 시퀀스용 테이블 만들어서 Key 생성관리. 모든 DB 가능하지만, 성능 낮아 운영환경에서 사용하기 부담스럽다
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
+//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
 
     // unique도 있지만 이름이 랜덤으로 지정되어 에러시 파악 어렵기에 안 씀
